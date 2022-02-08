194f336fbbe6094947e5d600f34c7f5bb9e60836
Umair Abbas <umairabbass786@gmail.com>
2022-02-09 00:08:27 +0500

    register.php completed

--- includes/nav.php
+++ includes/nav.php
@@ -1,7 +1,7 @@
 <!-- Side-Nav -->
 <div class="side-navbar active-nav d-flex justify-content-between flex-wrap flex-column" id="sidebar">
     <ul class="nav flex-column text-center w-100">
-      <a href="index.php" class="nav-link h3 my-2">
+      <a href="#" class="nav-link h3 my-2">
         <div class="d-flex">
           <img src="assets/img/logo.png" class="mr-3" alt="logo" width="120" height="100">
           CHAMPIONS </br>FIGHTER </br> UNION
@@ -43,7 +43,7 @@
     <nav class="navbar top-navbar navbar-light bg-transparent px-5">
       <a class="btn border-0" id="menu-btn"><i class="fas fa-bars"></i></a>
       <form class="d-flex signform" method="POST" action="#">
-        <a href="#" class="text-nowrap mt-2 mx-2">Create an Account</a>
+        <a href="register.php" class="text-nowrap mt-2 mx-2">Create an Account</a>
         <input class="form-control me-2" type="text" name="username" placeholder="Username">
         <input class="form-control me-2 mx-2" type="password" name="password" placeholder="Password">
         <!-- <a href="#" >Forget Password?</a> -->
